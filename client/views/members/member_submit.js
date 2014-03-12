2c6f3d696b3d34f97632fa933ced44498b73a604
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-12 16:03:45 -0500

    add prefix, suffix and affiliations to form

--- client/views/members/member_submit.js
+++ client/views/members/member_submit.js
@@ -3,9 +3,13 @@ Template.memberNew.events({
 		e.preventDefault();
 
 		var member = {
+			prefix: $(e.target).find('[name=prefix]').val(),
 			firstname: $(e.target).find('[name=firstname]').val(),
 			lastname: $(e.target).find('[name=lastname]').val(),
-			organization: $(e.target).find('[name=organization]').val()
+			suffix: $(e.target).find('[name=suffix]').val(),
+			institution1: $(e.target).find('[name=institution1]').val(),
+			institution2: $(e.target).find('[name=institution2]').val(),
+			institution3: $(e.target).find('[name=institution3]').val()
 		}
 
 		member._id = Members.insert(member);
