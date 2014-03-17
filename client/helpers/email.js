73d309cc19d0fa3bcd388d5f75c7e056fec66a5d
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-17 11:07:21 -0500

    add email helper function

---
+++ client/helpers/email.js
@@ -0,0 +1,12 @@
+var email = {
+	email: function() {
+	var user = Meteor.user();
+	if (user && user.emails)
+		return user.emails[0].address;
+	}
+}
+
+Template.signIn.helpers(email);
+Template.header.helpers(email);
+Template.signUp.helpers(email);
+Template.memberNew.helpers(email);
