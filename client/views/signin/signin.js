6f78bfda968cffdffa0abe2be1a6632bbeecfafc
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-17 11:07:00 -0500

    add custom signin and logout

---
+++ client/views/signin/signin.js
@@ -0,0 +1,32 @@
+Template.signIn.events({
+
+    'submit form' : function(e){
+      e.preventDefault();
+      // retrieve the input field values
+    
+        var email = $(e.target).find('[name=email]').val();
+        var password= $(e.target).find('[name=password]').val();
+  
+        // Trim and validate your fields:
+        var trimInput = function(val) {
+          return val.replace(/^\s*|\s*$/g, "");
+        }
+
+        email = trimInput(email);
+
+        // If validation passes, supply the appropriate fields to the
+        // Meteor.loginWithPassword() function.
+
+      Meteor.loginWithPassword(email, password, function(error) { 
+      if (error) {
+        throwError(error.reason);
+      } else {
+        Router.go('home');
+      }
+    });
+  },
+
+  'click #logout': function(){
+    Meteor.logout();
+  }
+});
