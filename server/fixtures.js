e2ba72e615dd9c91c56c8e9f0fab25616552f12a
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-17 13:39:04 -0500

    remove fixture data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,22 +1,22 @@
-if (Members.find().count() === 0) {
+// if (Members.find().count() === 0) {
 
-	Members.insert({
-		firstname: 'Tom',
-		lastname: 'Coleman',
-		institution1: 'meteor.com'
-	});	
+// 	Members.insert({
+// 		firstname: 'Tom',
+// 		lastname: 'Coleman',
+// 		institution1: 'meteor.com'
+// 	});	
 
-	Members.insert({
-		firstname: 'Rainbough',
-		lastname: 'Phillips',
-		institution1: 'Landes Bioscience'
-	});
-	Members.insert({
-		firstname: 'Jane',
-		lastname: 'Doe',
-		institution1: 'Landes Bioscience'
-	});
+// 	Members.insert({
+// 		firstname: 'Rainbough',
+// 		lastname: 'Phillips',
+// 		institution1: 'Landes Bioscience'
+// 	});
+// 	Members.insert({
+// 		firstname: 'Jane',
+// 		lastname: 'Doe',
+// 		institution1: 'Landes Bioscience'
+// 	});
 
 	
-}
+// }
 
