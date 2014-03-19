d3bdb4852a29d3c0427a46f11be4baebefcf15c3
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-19 14:22:00 -0500

    add server methods for admin route hook

---
+++ server/admin.js
@@ -0,0 +1,10 @@
+Meteor.methods({
+	isAdmin: function(user){
+	
+		if(!Roles.userIsInRole(user, 'admin'))
+			throw new Meteor.Error(403, "You are not authorized to view this page!");
+
+		return user._id;
+
+	}
+});
