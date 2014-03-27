7657a07077c6c95bd0f83e4fead5a4d3e41a5cc0
Rainbough Phillips <rainbough@landesbioscience.com>
2014-03-27 15:41:18 -0500

    add comments

--- client/views/groups/group_submit.js
+++ client/views/groups/group_submit.js
@@ -1,7 +1,7 @@
 Template.newGroup.events({
 	'submit form': function(e){
 		e.preventDefault();
-
+		//use jquery to pull data from form
 		var group = {
 			groupName: $(e.target).find("[name=groupName]").val(),
 			user: $(e.target).find("[name=user]").val()
@@ -10,7 +10,7 @@ Template.newGroup.events({
 		Meteor.call('newGroup', group, function(error, id){
 			if (error) {
 					throwError(error.reason);
-					
+					// redirect if group already exists
 					if (error.error === 302)
 						Router.go('groupPage', {groupName: error.details})
 				} else {
