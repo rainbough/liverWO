07a21333c05e76acfb7b975c3d1735b93dd74a51
Rainbough Phillips <rainbough@landesbioscience.com>
2014-04-01 13:55:18 -0500

    create new form elements and array inputs

--- client/views/admin/admin_member_new.js
+++ client/views/admin/admin_member_new.js
@@ -1,9 +1,13 @@
 Template.adminMemberNew.events({
 	'submit form': function(e) {
 		e.preventDefault();
+		// Each one of these attributes should have an associated form input on admin_member_new.html
+		// Try to keep them in the same order they appear on html form
 
 		var member = {
 			routeName: $(e.target).find('[name=routeName]').val(),
+			imageUrl: $(e.target).find('[name=image_url]').val(),
+			email2: $(e.target).find('[name=email2]').val(),
 			title: $(e.target).find('[name=title]').val(),
 			prefix: $(e.target).find('[name=prefix]').val(),
 			firstname: $(e.target).find('[name=firstname]').val(),
@@ -12,7 +16,6 @@ Template.adminMemberNew.events({
 			institution1: $(e.target).find('[name=institution1]').val(),
 			institution2: $(e.target).find('[name=institution2]').val(),
 			institution3: $(e.target).find('[name=institution3]').val(),
-			imageUrl: $(e.target).find('[name=image_url]').val(),
 			email: $(e.target).find('[name=email]').val(),
 			labPhone: $(e.target).find('[name=phone]').val(),
 			labName: $(e.target).find('[name=labName]').val(),
@@ -21,9 +24,15 @@ Template.adminMemberNew.events({
 			city: $(e.target).find('[name=city]').val(),
 			state: $(e.target).find('[name=state]').val(),
 			zip: $(e.target).find('[name=zip]').val(),
-			country: $(e.target).find('[name=country]').val()
+			country: $(e.target).find('[name=country]').val(),
+			RIkeywords: $(e.target).find('[name=RIkeywords]').val(),
+			newitem: $(e.target).find('[name=newitem]').val(),
+			newitem2: $(e.target).find('[name=newitem2]').val(),
+			labAssociates: getArray('associates')
 		}
-
+	
+			
+// This calls a method in collections/members.js
 		Meteor.call('member', member, function(error, id) { 
 			if (error) {
 				throwError(error.reason);
@@ -38,3 +47,13 @@ Template.adminMemberNew.events({
 		});
 	}
 });
+
+// This returns an array of values for inputs with an associated class
+var getArray = function(class_name){
+	var input_array = [];
+	$('.'+ class_name).each(function(){
+		input_array.push($(this).val());
+	});
+	return input_array
+
+}
