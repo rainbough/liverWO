553a55d41ac6da8c36d8d7004da64d29be74e649
rainbough <rainbough@gmail.com>
2014-05-09 10:28:04 -0500

    adjust list for newest sort

--- client/views/articles/articleList.js
+++ client/views/articles/articleList.js
@@ -1,34 +1,9 @@
 /// articleList ///
 
 Template.articleList.articles = function(){
-    return Articles.find();
+    return Articles.find({},{ sort: {retrived: -1}});
 };
 
-// Template.articleList.events({
-//     'click input.removeArticle': function(){
-//         Articles.remove(this._id);
-//     },
-//     'click input.removeAll': function(){
-//         Meteor.call('removeAll', done);
-//     },
-//     // 'keydown input' : function(event){
-//     //     console.log('key', event);
-//     //     if(event.which == 13){
-//     //         Session.set('previous_article', Session.get('article_open'));
-//     //         Meteor.call('markRead', Session.get('article_open'), done);
-//     //         Session.set('article_open', $( "#"+Session.get('article_open').next().id() ));
-//     //     }
-
-//     //     $('body').on('keydown',function(){
-//     //         console.log('key pressed');
-//     //     });
-//     // }
-// });
-
-// Template.articleList.rendered = function(){
-//     var divId = '#' + Session.get('article_open');
-//     $('html, body').scrollTop($(divId).offset().top - 50);
-// };
 
 Template.articleList.rendered = function(){
     Meteor.call('refreshFeed', function(err, id){
@@ -36,3 +11,22 @@ Template.articleList.rendered = function(){
             throwError(err.reason);
     });
 };
+
+Template.articleList.events({
+	"click #editFeed": function(e){
+		e.preventDefault();
+
+		var newUrl = $(e.target).find('#feedUrl').val();
+
+		Meteor.call('editFeed', newUrl, function(err, id){
+			if(err){
+				throwError(err.reason);
+			}else{
+				Meteor.call('refreshFeed', function(err, id){
+		        	if(err)
+		           		 throwError(err.reason);
+		    		});
+			}
+		});
+	}
+});
