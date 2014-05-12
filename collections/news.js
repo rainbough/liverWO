10a9e16b89df9b4effd6e917912a68509a389975
rainbough <rainbough@gmail.com>
2014-05-12 12:59:47 -0500

    add meteor method for news items

--- collections/news.js
+++ collections/news.js
@@ -4,3 +4,33 @@ News.allow({
 	update: adminUser,
 	remove: adminUser
 });
+
+Meteor.methods({
+	news: function(newsAttributes) {
+		
+		var trimInput = function(val) {
+        	return val.replace(/\s+/g,'');
+      	}
+      	var trimRouteName = trimInput(newsAttributes.title);
+
+      	var uniqueRouteName = News.findOne({routeName: trimRouteName});
+
+		if(!newsAttributes.title)
+			throw new Meteor.Error(422, "Please Enter a title.")
+
+		if (!newsAttributes.content)
+			throw new Meteor.Error(422, 'Please enter some content for this news item.');
+
+		if(uniqueRouteName)
+			throw new Meteor.Error(301, 'The route name ' + trimRouteName + " already exists.");
+		
+		var newsItem = _.extend(_.pick(newsAttributes, 'title', 'content', 'imageUrl'), {
+			submitted: new Date().getTime(),
+			routeName: trimRouteName
+		});
+
+		var newsId = News.insert(newsItem);
+
+		return newsId;
+	}
+});
