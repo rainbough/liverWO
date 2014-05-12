433b98855a10aa1938616578ec670d14a3c2ca3d
rainbough <rainbough@gmail.com>
2014-05-12 11:22:53 -0500

    add admin news form

--- client/views/admin/admin_news.js
+++ client/views/admin/admin_news.js
@@ -1,3 +1,17 @@
 Template.adminNews.rendered = function(){
 	$('#content').redactor();
 };
+
+Template.adminNews.events({
+	'submit form': function(e){
+		e.preventDefault();
+
+		var news = {
+			title: $(e.target).find('[name=title]').val(),
+			content: $(e.target).find('#content').val(),
+			imageUrl: $(e.target).find('[name=imageUrl]').val()
+		}
+
+		console.log(news.content);
+	}
+});
