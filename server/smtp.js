1ba6cade12f704777ccbc7f3a16e8a8a35866f87
Rainbough Phillips <rainbough@gmail.com>
2014-05-16 13:30:49 -0500

    Delete smtp.js

--- server/smtp.js
+++
@@ -1,5 +0,0 @@
-// This is the smtp credentials for the mandrill api
-
-Meteor.startup(function () {
-	process.env.MAIL_URL = 'smtp://app22908635@heroku.com:KyUC3p4TFFAKeDYnF-0p9Q@smtp.mandrillapp.com:587';
-});
