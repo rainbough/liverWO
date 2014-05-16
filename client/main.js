1ec400d426eec9fb6d469b47cfe5e922c843ab71
Amanda Landes Macbook3 <asaffer@landesbioscience.com>
2014-05-16 11:24:40 -0500

    third big design push. now comes functionality

---
+++ client/main.js
@@ -0,0 +1,10 @@
+Router.load(function(){
+	document.body.scrollTop = document.documentElement.scrollTop = 0;
+	/*$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	});*/
+});
