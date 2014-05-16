518c2c2669df8bb347effc377e01c8cbe2c641b3
Amanda Landes Macbook3 <asaffer@landesbioscience.com>
2014-05-16 12:05:59 -0500

    clean up

--- client/views/includes/contact.js
+++
@@ -1,8 +0,0 @@
-Template.static_contact.events({
-	"click button#contact-submit": function(e) {
-		e.preventDefault();
-		var fdata = $("form.contact").serializeFormToObj();
-		Meteor.call("sendContactEmail",fdata.message,fdata.email,fdata.first_name,function(err){
-		});
-	}
-});
