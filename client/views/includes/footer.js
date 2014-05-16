e60faddb03f18728729611ceda836e967566d097
Amanda Landes Macbook3 <asaffer@landesbioscience.com>
2014-05-16 13:42:02 -0500

    oops footer.js was messed up

--- client/views/includes/footer.js
+++ client/views/includes/footer.js
@@ -2,6 +2,9 @@ Template.base.events({
 	"click #mailchimp-subscribe-btn": function(e){
 		e.preventDefault();
 		var sub_email = $("#email-address").val();
-		Meteor.call("subscribeMailchimp",sub_email);
+		Meteor.call("subscribeMailchimp",sub_email,function(err,res){
+			if(err){alert("Error!\n"+err.reason);}
+			else {alert("Success!\nYou will receive a confirmation email shortly.");}
+		});
 	}
 });
